b4363fd9675373c477ed63772a60d18cf619016a
hossam helmy <hossammohamedhelmy@gmail.com>
2021-07-21 01:57:45 +0200

    fix error

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '1011835f-e086-4e7b-85e5-57913b890ff6';
 export const MODAL_CLOSE_SEC = 2.5;
